1fcff478ce9e22054059912ec40232ea69c22a27
Sudheer Kumar <sudheer@nextcrafter.com>
2025-03-09 18:17:02 +0530

    Refactor GET user API route to remove request parameter and directly call auth function

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { auth } from "@/auth"
 
-export async function GET(req: Request) {
+export async function GET() {
     try {
         const session = await auth()
         if (session) {
